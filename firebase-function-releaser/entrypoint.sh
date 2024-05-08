4ed05162d9c83c14295c6fd555830e693c2b850a
timam <mail@timam.io>
2024-05-08 12:36:55 +0600

    Updated Firebase authentication in deployment script
    The Firebase deployment script has been updated to use the provided Firebase token when setting the Firebase project. The change ensures that the correct authentication is used during both setting the project and deploying the functions. This change will greatly improve the reliability and security of the deployment process.

--- firebase-function-releaser/entrypoint.sh
+++ firebase-function-releaser/entrypoint.sh
@@ -5,7 +5,7 @@ PROJECT_ID=$1
 FIREBASE_TOKEN=$2
 
 echo "setting firebase project to $PROJECT_ID"
-firebase use --add "$PROJECT_ID"
+firebase use --add "$PROJECT_ID" --token "$FIREBASE_TOKEN"
 
-echo "deploying functions $PROJECT_ID"
-firebase deploy --only functions:retryOrder,functions:processCancelPayment --token $FIREBASE_TOKEN
+echo "deploying functions"
+firebase deploy --only functions:retryOrder,functions:processCancelPayment --token "$FIREBASE_TOKEN"
