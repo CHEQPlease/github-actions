b4d794917da5c4ba94de1ed9344194662311bda0
timam <mail@timam.io>
2024-05-08 12:12:32 +0600

    Update entrypoint.sh to deploy specific firebase functions
    In firebase-function-releaser/entrypoint.sh, the deployment script was updated. Now it deploys only retryOrder, processCancelPayment, and processCustomerSignature functions. Furthermore, the directory listing was changed to list only the functions directory.

--- firebase-function-releaser/entrypoint.sh
+++ firebase-function-releaser/entrypoint.sh
@@ -4,4 +4,6 @@ set -e
 PROJECT_ID=$1
 echo "PROJECT_ID is: $PROJECT_ID"
 pwd
-ls -larth
+ls -larth functions
+
+firebase deploy --only functions:retryOrder,functions:processCancelPayment,functions:processCustomerSignature
