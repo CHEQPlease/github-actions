3d04c2921ea539f78e0e283d2f06c4da4d0d320c
timam <mail@timam.io>
2024-05-08 13:06:16 +0600

    Removed token usage in Firebase deployment
    The Firebase token was removed from the command lines used for setting the Firebase project and deploying the functions. This revision ensures that the Firebase project setting and deployment operations do not rely on explicitly passed tokens.

--- firebase-function-releaser/entrypoint.sh
+++ firebase-function-releaser/entrypoint.sh
@@ -8,7 +8,7 @@ echo "${FIREBASE_CREDENTIALS}" > service-account.json
 export GOOGLE_APPLICATION_CREDENTIALS="./service-account.json"
 
 echo "setting firebase project to $PROJECT_ID"
-firebase use --add "$PROJECT_ID" --token "$FIREBASE_TOKEN"
+firebase use --add "$PROJECT_ID"
 
 echo "deploying functions"
-firebase deploy --only functions:retryOrder,functions:processCancelPayment --token "$FIREBASE_TOKEN"
+firebase deploy --only functions:retryOrder,functions:processCancelPayment
